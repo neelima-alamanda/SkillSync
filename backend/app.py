9c8950cb573ecb9b7db88bb6ece9f91638292596
coder-uma72 <umareddymarrivada@gmail.com>
2026-09-05 02:03:47 +0530

    feat: add job API routes

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from backend.auth.auth_routes import auth_bp
 from backend.config import Config
 from backend.models import db
 from backend.routes.student_routes import student_bp
+from backend.routes.job_routes import job_bp
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +12,7 @@ def create_app():
     db.init_app(app)
     CORS(app)
     app.register_blueprint(student_bp)
+    app.register_blueprint(job_bp)
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     @app.get("/api/health")
     def health_check():
